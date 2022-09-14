fff29f14de9d98bc9bf43b49ecb753077ff38a78
Kshitij Srivastava <46964552+KshitijCodes@users.noreply.github.com>
2022-09-14 13:52:25 +0530

    Create leetcode9.cpp
    Palindrome Numbers

---
+++ leetcode9.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long rev=0;
+        int temp=x;
+        while(x>0){
+            rev = rev*10 + (x % 10);
+            x = x / 10;
+        }
+        if(temp==rev) return true;
+        else return false;
+    }
+};
