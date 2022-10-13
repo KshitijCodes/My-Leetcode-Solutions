370f1f2893bb41375b0f02da195a529aecfe659c
Kshitij Srivastava <46964552+KshitijCodes@users.noreply.github.com>
2022-10-13 20:49:43 +0530

    Create leetcode0073.cpp
    Set Matrix Zeroes

---
+++ leetcode0073.cpp
@@ -0,0 +1,24 @@
+//Set Matrix Zeroes
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+	set<int> rows, cols;
+    for(int i=0; i<matrix.size(); i++){
+        for(int j=0; j<matrix[0].size(); j++){
+            if(matrix[i][j]==0){
+                rows.insert(i);
+                cols.insert(j);
+            }
+        }
+    }
+    
+    for(int i=0; i<matrix.size(); i++){
+        for(int j=0; j<matrix[0].size(); j++){
+            if(rows.count(i)||cols.count(j)){
+                matrix[i][j]=0;
+            }
+        }
+    }
+}
+};
